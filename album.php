c5657029e573ca669bc14d850c5f6b1ec06ab545
tiger-guagua <ert24251334@gmail.com>
2024-05-20 16:13:05 +0800

    調整 introduce.php 的排版、新增 header.php 將 header 相關 code移入 檔案的 header 改成使用 php 的 include once 方式引入

---
+++ album.php
@@ -0,0 +1,33 @@
+<?php include_once "header.php"; ?>
+<style>
+    /* 样式表中设置网格布局 */
+    .grid-container {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr); /* 4列 */
+        grid-gap: 30px; /* 图片之间的间隔 */
+    }
+    .grid-item img {
+        width: 100%; /* 图片宽度占据其容器的100% */
+        height: auto; /* 根据宽度自动调整高度，保持图片比例 */
+    }
+</style>
+<body class="p-3 m-0 border-0 bd-example m-0 border-0">
+    <div class="grid-container">
+        <div class="grid-item"><img src="img/DSC00773.jpg" alt="/img/DSC00773.jpg"></div>
+        <div class="grid-item"><img src="img/DSC09060.jpg" alt="/img/DSC09060.jpg"></div>
+        <div class="grid-item"><img src="img/DSC03979.jpg" alt="/img/DSC03979.jpg"></div>
+        <div class="grid-item"><img src="img/DSC04387.jpg" alt="/img/DSC04387.jpg"></div>
+        <div class="grid-item"><img src="img/DSC04849.jpg" alt="/img/DSC04849.jpg"></div>
+        <div class="grid-item"><img src="img/DSC04883.jpg" alt="/img/DSC04883.jpg"></div>
+        <div class="grid-item"><img src="img/DSC04995.jpg" alt="/img/DSC04995.jpg"></div>
+        <div class="grid-item"><img src="img/DSC05392.jpg" alt="/img/DSC05392.jpg"></div>
+        <div class="grid-item"><img src="img/DSC06229.jpg" alt="/img/DSC06229.jpg"></div>
+        <div class="grid-item"><img src="img/DSC06253.jpg" alt="/img/DSC06253.jpg"></div>
+        <div class="grid-item"><img src="img/DSC07624.jpg" alt="/img/DSC07624.jpg"></div>
+        <div class="grid-item"><img src="img/DSC07639.jpg" alt="/img/DSC07639.jpg"></div>
+        <div class="grid-item"><img src="img/DSC08048.jpg" alt="/img/DSC08048.jpg"></div>
+        <div class="grid-item"><img src="img/DSC08137.jpg" alt="/img/DSC08137.jpg"></div>
+        <div class="grid-item"><img src="img/DSC08235.jpg" alt="/img/DSC08235.jpg"></div>
+        <div class="grid-item"><img src="img/DSC08620.jpg" alt="/img/DSC08620.jpg"></div>
+    </div>
+</body>
--- introduce.php
+++ introduce.php
@@ -1,150 +1,128 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-    <title>Sea ya Cafe</title>
-    <style>
+<?php include_once "header.php"; ?>
+<style>
     body{
-            color: white;
-        }
-  /* 样式表中设置网格布局 */
-  .grid-container {
-    display: grid;
-    grid-template-columns: repeat(4, 1fr); /* 4列 */
-    grid-gap: 10px; /* 图片之间的间隔 */
-  }
+        color: white;
+    }
+    /* 样式表中设置网格布局 */
+    .grid-container {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr); /* 4列 */
+        grid-gap: 10px; /* 图片之间的间隔 */
+    }
 
-  .grid-item img {
-    width: 100%; /* 图片宽度占据其容器的100% */
-    height: auto; /* 根据宽度自动调整高度，保持图片比例 */
+    .grid-item img {
+        width: 100%; /* 图片宽度占据其容器的100% */
+        height: auto; /* 根据宽度自动调整高度，保持图片比例 */
 
-  }
+    }
 
+    .flex-container {
+        display: flex;
+        justify-content: space-around;
+        padding:10px;
+        flex-direction:row;
+        flex-wrap: wrap;
+        align-items: stretch;
+    }
+    .item {
+        color:white;
+        font-size:2rem;
+        padding:10px;
+    }
 
-  .flex-container {
-  display: flex;
-  justify-content: space-around;
-  padding:10px;
-  flex-direction:row;
-  flex-wrap: wrap;
-  align-items: stretch;
-}
- .item {
-  width:;
-  margin:;
-  justigy-content:center;
-  align-items:;
-  color:white;
-  font-size:2rem;
-  padding:10px;
- }
+    .banner-container{
+        background-color:#f7f1dc ;
+        text-align: center;
+    }
 
- .banner-container{
-   background-color:#f7f1dc ;
-   text-align: center;
- }
+    .blue{
+        background-color:#394766;
+    }
 
- .blue{
-    background-color:#394766;
- }
-
- .center{
-    text-align: center;
- }
-
+    .center{
+        text-align: center;
+    }
 </style>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-  </head>
-  <body class="p-3 m-0 border-0 bd-example m-0 border-0">    
-        
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="http://localhost:8888/Seaya%20Cafe.php"><img src="/img/logo_工作區域 1.png" alt="/img/logo_工作區域 1.png" width="250px"></a>
-        <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="navbar-collapse collapse" id="navbarSupportedContent" style="">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="http://localhost:8888/Seaya%20Cafe.php">關於Sea ya</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="http://localhost:8888/introduce.php">introduce</a>
-              <li class="nav-item">
-              <a class="nav-link" href="http://localhost:8888/img.php#">img</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-            </li>
-          </ul>
-          <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
-        </div>
-      </div>
-    </nav>
-
+<body class="p-3 m-0 border-0 bd-example m-0 border-0">
     <div class="banner-container">
-        <img class="" id="logo"src="/img/logo_工作區域 1.png" alt="seaya_logo"width="400px">
+        <img src="img/navbar_logo.png" alt="海見咖啡" width="400px">
     </div>
-<div class="blue">
-    <div class="flex-container">
-        <div class="item" >
-            <img src="/img/150ppi/暖流配方.png">
-            <p>
-                <br>處理法：水洗
-                <br>產地：哥倫比亞/坦尚尼亞/瓜地馬拉
-                <br>有紅糖的些許發酵香氣，帶有楓糖甜感、
-                <br>烤榛果與牛奶巧克力的風味
-            </p>
-            </p>
-        </div>
-        <div class="item" >
-            <img src="/img/150ppi/衝浪配方.png">
-               <p>
-                <br>處理法：日曬、水洗
-                <br>產地：衣索比亞/瓜地馬拉
-                <br>明亮柑橘酸質、檸檬糖甜感，
-                <br>呈現豐富水果調性
-                </p>
-            </div>
-    </div>
-        <div class="center">------</div>
+    <div class="blue">
         <div class="flex-container">
             <div class="item">
-                <img src="/img/150ppi/custom.png">
+                <img src="img/150ppi/暖流配方.png">
+                <p>
+                    <br>處理法：水洗
+                    <br>產地：哥倫比亞/坦尚尼亞/瓜地馬拉
+                    <br>有紅糖的些許發酵香氣，帶有楓糖甜感、
+                    <br>烤榛果與牛奶巧克力的風味
+                </p>
+                </p>
+            </div>
+            <div class="item" >
+                <img src="/img/150ppi/衝浪配方.png">
+                <p>
+                    <br>處理法：日曬、水洗
+                    <br>產地：衣索比亞/瓜地馬拉
+                    <br>明亮柑橘酸質、檸檬糖甜感，
+                    <br>呈現豐富水果調性
+                </p>
+            </div>
+        </div>
+        <div class="center">------</div>
+        <!-- <div class="flex-container">
+            <div class="item">
+                <img src="img/150ppi/custom.png">
                 <h1>想要更多特製？</h1>
             </div>
 
             <div class="item">
-            <ul>
-            <li><h1>加一份濃縮</h1></li>
-            <br>
-            <li><h1>換燕麥奶</h1></li>
-            </ul>
-            </div>
-
-            <div class="item">
-            <ul>
-                <img src="/img/150ppi/+40.png">
+                <ul>
+                    <li><h1>加一份濃縮</h1></li>
                 <br>
-                <img src="/img/150ppi/+20.png">
-            </ul>
+                    <li><h1>換燕麥奶</h1></li>
+                </ul>
+            </div>
+            <div class="item">
+                <ul>
+                    <img src="img/150ppi/+40.png">
+                    <br>
+                    <img src="img/150ppi/+20.png">
+                </ul>
+            </div>
+        </div> -->
+        <div class="row">
+            <div class="col text-center">
+                <img src="img/150ppi/custom.png">
+                <h1>想要更多特製？</h1>
+            </div>
+            <div class="col">
+                <div class="row">
+                    <div class="col">
+                        <div class="row align-items-center">
+                            <div class="col">
+                                <li class="h1">加一份濃縮</li>
+                            </div>
+                            <div class="col">
+                                <img src="img/150ppi/+40.png">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="row align-items-center">
+                            <div class="col">
+                            <li class="h1">換燕麥奶</li>
+                            </div>
+                            <div class="col">
+                                <img src="img/150ppi/+20.png">
+                            </div>
+                        </div>
+                        <!-- <span class="h1">換燕麥奶</span>
+                        <img src="img/150ppi/+20.png"> -->
+                    </div>
+                </div>
             </div>
         </div>
         <div class="center">------</div>
@@ -152,6 +130,5 @@
             <div class="item"><p>單品豆、餐點請洽櫃檯<br>自備環保杯折扣 $5</p>
             </div>
         </div>
-</div>
-
+    </div>
 </body>
