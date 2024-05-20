da7de112db16614a1b8b341ddd23a2d20f3defb0
tiger-guagua <ert24251334@gmail.com>
2024-05-20 16:52:13 +0800

    將 header 改成引入檔

--- about.php
+++ about.php
@@ -1,67 +1,19 @@
-!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-    <title>Sea ya Cafe</title>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-  </head>
-  <body class="p-3 m-0 border-0 bd-example m-0 border-0">    
-        
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="http://localhost:8888/Seaya%20Cafe.php"><img src="/img/logo_工作區域 1.png" alt="/img/logo_工作區域 1.png" width="250px"></a>
-        <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="navbar-collapse collapse" id="navbarSupportedContent" style="">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="http://localhost:8888/Seaya%20Cafe.php">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="http://localhost:8888/img.php#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-            </li>
-          </ul>
-          <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
+<?php include_once "header.php"; ?>
+<body class="p-3 m-0 border-0 bd-example m-0 border-0">
+    <form>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
         </div>
-      </div>
-    </nav>
-    
-<form>
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-      </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
-      </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
     </form>
-    
-  </body>
+</body>
--- album.php
+++ album.php
@@ -13,21 +13,21 @@
 </style>
 <body class="p-3 m-0 border-0 bd-example m-0 border-0">
     <div class="grid-container">
-        <div class="grid-item"><img src="img/DSC00773.jpg" alt="/img/DSC00773.jpg"></div>
-        <div class="grid-item"><img src="img/DSC09060.jpg" alt="/img/DSC09060.jpg"></div>
-        <div class="grid-item"><img src="img/DSC03979.jpg" alt="/img/DSC03979.jpg"></div>
-        <div class="grid-item"><img src="img/DSC04387.jpg" alt="/img/DSC04387.jpg"></div>
-        <div class="grid-item"><img src="img/DSC04849.jpg" alt="/img/DSC04849.jpg"></div>
-        <div class="grid-item"><img src="img/DSC04883.jpg" alt="/img/DSC04883.jpg"></div>
-        <div class="grid-item"><img src="img/DSC04995.jpg" alt="/img/DSC04995.jpg"></div>
-        <div class="grid-item"><img src="img/DSC05392.jpg" alt="/img/DSC05392.jpg"></div>
-        <div class="grid-item"><img src="img/DSC06229.jpg" alt="/img/DSC06229.jpg"></div>
-        <div class="grid-item"><img src="img/DSC06253.jpg" alt="/img/DSC06253.jpg"></div>
-        <div class="grid-item"><img src="img/DSC07624.jpg" alt="/img/DSC07624.jpg"></div>
-        <div class="grid-item"><img src="img/DSC07639.jpg" alt="/img/DSC07639.jpg"></div>
-        <div class="grid-item"><img src="img/DSC08048.jpg" alt="/img/DSC08048.jpg"></div>
-        <div class="grid-item"><img src="img/DSC08137.jpg" alt="/img/DSC08137.jpg"></div>
-        <div class="grid-item"><img src="img/DSC08235.jpg" alt="/img/DSC08235.jpg"></div>
-        <div class="grid-item"><img src="img/DSC08620.jpg" alt="/img/DSC08620.jpg"></div>
+        <div class="grid-item"><img src="img/DSC00773.jpg" alt="img/DSC00773.jpg"></div>
+        <div class="grid-item"><img src="img/DSC09060.jpg" alt="img/DSC09060.jpg"></div>
+        <div class="grid-item"><img src="img/DSC03979.jpg" alt="img/DSC03979.jpg"></div>
+        <div class="grid-item"><img src="img/DSC04387.jpg" alt="img/DSC04387.jpg"></div>
+        <div class="grid-item"><img src="img/DSC04849.jpg" alt="img/DSC04849.jpg"></div>
+        <div class="grid-item"><img src="img/DSC04883.jpg" alt="img/DSC04883.jpg"></div>
+        <div class="grid-item"><img src="img/DSC04995.jpg" alt="img/DSC04995.jpg"></div>
+        <div class="grid-item"><img src="img/DSC05392.jpg" alt="img/DSC05392.jpg"></div>
+        <div class="grid-item"><img src="img/DSC06229.jpg" alt="img/DSC06229.jpg"></div>
+        <div class="grid-item"><img src="img/DSC06253.jpg" alt="img/DSC06253.jpg"></div>
+        <div class="grid-item"><img src="img/DSC07624.jpg" alt="img/DSC07624.jpg"></div>
+        <div class="grid-item"><img src="img/DSC07639.jpg" alt="img/DSC07639.jpg"></div>
+        <div class="grid-item"><img src="img/DSC08048.jpg" alt="img/DSC08048.jpg"></div>
+        <div class="grid-item"><img src="img/DSC08137.jpg" alt="img/DSC08137.jpg"></div>
+        <div class="grid-item"><img src="img/DSC08235.jpg" alt="img/DSC08235.jpg"></div>
+        <div class="grid-item"><img src="img/DSC08620.jpg" alt="img/DSC08620.jpg"></div>
     </div>
 </body>
